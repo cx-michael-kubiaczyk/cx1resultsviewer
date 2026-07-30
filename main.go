8327b1415058a5ca0a388a7a31da004fb5cc67a3
Michael Kubiaczyk <48311127+cx-michael-kubiaczyk@users.noreply.github.com>
2026-07-30 09:27:43 +0200

    test flow runs

---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"crypto/tls"
+	"cx1resultsviewer/internal/backend"
+	"flag"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/cxpsemea/Cx1ClientGo"
+	"github.com/sirupsen/logrus"
+	easy "github.com/t-tomalak/logrus-easy-formatter"
+)
+
+func main() {
+	logger := logrus.New()
+	logger.SetLevel(logrus.TraceLevel)
+	myformatter := &easy.Formatter{}
+	myformatter.TimestampFormat = "2006-01-02 15:04:05.000"
+	myformatter.LogFormat = "[%lvl%][%time%] %msg%\n"
+	logger.SetFormatter(myformatter)
+	// Use stderr so logs don't corrupt the MCP stdio transport on stdout.
+	logger.SetOutput(os.Stderr)
+
+	logger.Info("Starting")
+	LogLevel := flag.String("log", "INFO", "Log level: TRACE, DEBUG, INFO, WARNING, ERROR, FATAL")
+
+	httpClient := &http.Client{}
+	if false {
+		proxyURL, _ := url.Parse("http://127.0.0.1:8080")
+		transport := &http.Transport{}
+		transport.Proxy = http.ProxyURL(proxyURL)
+		transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		httpClient.Transport = transport
+		logger.Infof("Using proxy")
+	}
+
+	cx1client, err := Cx1ClientGo.NewClient(httpClient, logger)
+	if err != nil {
+		logger.Fatalf("Error creating client: %s", err)
+	}
+	logger.Infof("Connected with %v", cx1client.String())
+
+	switch strings.ToUpper(*LogLevel) {
+	case "TRACE":
+		logger.SetLevel(logrus.TraceLevel)
+	case "DEBUG":
+		logger.SetLevel(logrus.DebugLevel)
+	case "INFO":
+		logger.SetLevel(logrus.InfoLevel)
+	case "WARNING":
+		logger.SetLevel(logrus.WarnLevel)
+	case "ERROR":
+		logger.SetLevel(logrus.ErrorLevel)
+	case "FATAL":
+		logger.SetLevel(logrus.FatalLevel)
+	}
+
+	server := backend.NewServer(cx1client, logger)
+	defer server.Shutdown()
+
+	if err = server.Run(); err != nil {
+		logger.Errorf("Failed while running server: %s", err)
+	}
+
+	logger.Info("Done!")
+}
